3a212aff5e8536c3fe214002a1113b9cdddb6af4
huypd <huypd@vng.com.vn>
2024-11-27 11:13:46 +0700

    update redis ttl

--- src/main/java/com/example/demo/config/CacheConfiguration.java
+++ src/main/java/com/example/demo/config/CacheConfiguration.java
@@ -2,19 +2,20 @@ package com.example.demo.config;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
-//    @Bean
-//    public RedissonClient redissonClient() throws IOException {
-//        Config config = Config.fromYAML(new ClassPathResource("redisson-jcache.yaml").getInputStream());
-//        config.setCodec(new org.redisson.codec.SerializationCodec());
-//        return Redisson.create(config);
-//    }
-//
-//    @Bean
-//    public CacheManager cacheManager(RedissonClient redissonClient) {
-//        return new RedissonSpringCacheManager(redissonClient);
-//    }
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(300)).disableCachingNullValues())
+                .build();
+    }
 }
